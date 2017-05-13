3f293ce37d265bb52749dc4dbee7524d9d624017
Shayan <sh.taslim@gmail.com>
2017-05-13 15:33:19 -0400

    Added verbose logging system

--- MunitiLogger/Muniti.swift
+++ MunitiLogger/Muniti.swift
@@ -11,13 +11,49 @@ import Foundation
 class Muniti {
     
     let statics : MunitiStats
+    typealias JSON = [String : Any]
     
     init(){
         self.statics = MunitiStats()
     }
     
-    func registerSession() {
-        self.statics.registerSession()
+    /// this is the log funtion with types
+    func log(_ text : String, type : conditions) {
+        self.log(text, type: type.rawValue)
     }
+    
+    /// this is the general log funtion
+    func log(_ text : String, type : String = "") {
+        // here we will send the data with user id to the server
+        
+        // this is a sample json file
+        let json : JSON = ["uid" : "self.uid", "log" : text, "type" : type, "date" : statics.unix]
+    }
+    
+    func warning(_ text : String) {
+        log(text, type: .warning)
+    }
+    
+    func error(_ text : String) {
+        log(text, type: .error)
+    }
+    
+    func newUser(_ text : String) {
+        log(text, type: .newUser)
+    }
+    
+    func message(_ text : String) {
+        log(text, type: .message)
+    }
+    
+    public enum conditions : String {
+        case warning = "⚠️"
+        case error = "😱"
+        case message = "💬"
+        case newUser = "🎉"
+        case log = "🤖"
+        case none = ""
+    }
+    
     
 }
